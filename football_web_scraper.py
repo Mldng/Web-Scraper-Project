1261565c632d6eb8563d7923a0daa92ee068be98
duong nguyen <melody.duong.nguyen@gmail.com>
2023-12-06 13:29:39 +0900

    add docstring and comment for get_data_ENG

--- football_web_scraper.py
+++ football_web_scraper.py
@@ -6,20 +6,49 @@ import time
 import os
 
 def get_data_ENG(url) -> pd:
+    """
+    Scrapes football match data from a given URL
+    and returns it as a pandas DataFrame.
+
+    This function opens the given URL in a Chrome WebDriver,
+    then it locates and clicks on the 'All matches' button.
+    It then extracts data for each match, including date,
+    home team, score, and away team,
+    and compiles this information into a pandas DataFrame.
+
+    Args:
+        url (str): The URL of the website to scrape for match data.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the scraped data with columns for date,
+                      home team, score, and away team.
+
+    Note:
+        This function assumes that the webpage structure contains specific elements
+        identifiable by given XPATHs and tags. Changes in the web page's structure may
+        require updates to the locators used in this function.
+    """
+
+    # Initialize the Chrome WebDriver
     driver = webdriver.Chrome()
     driver.get(url)
 
+    # Find and click the 'All matches' button
     all_matches_btn = (driver.find_element(By.XPATH,
                                            '//label[@analytics-event="All matches"]')
                        )
     all_matches_btn.click()
+
+    # Find all match elements
     matches = driver.find_elements(By.TAG_NAME,'tr')
 
+    # Initialize lists to store match data
     dates = []
     home_teams = []
     scores = []
     away_teams = []
 
+    # Iterate through each match and extract data
     for match in matches:
         date = match.find_element(By.XPATH,'./td[1]').text
         home_team = match.find_element(By.XPATH,'./td[2]').text
@@ -30,7 +59,11 @@ def get_data_ENG(url) -> pd:
         home_teams.append(home_team)
         scores.append(score)
         away_teams.append(away_team)
+
+    # Quit the WebDriver session
     driver.quit()
+
+    # Create a DataFrame from the extracted data
     df = pd.DataFrame({
         'dates': dates,
         'home_teams': home_teams,
