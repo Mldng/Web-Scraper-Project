47065a1e2809d4ac3ef5796d0a20029425bb336c
duong nguyen <melody.duong.nguyen@gmail.com>
2023-12-06 13:32:20 +0900

    update docstring and cmm for get_data_dropdown

--- football_web_scraper.py
+++ football_web_scraper.py
@@ -73,30 +73,63 @@ def get_data_ENG(url) -> pd:
     return df
 
 def get_data_dropdown(url,index) -> pd:
+    """
+    Scrapes football match data for a specific country from a given URL and
+    returns it as a pandas DataFrame.
+
+    This function opens the given URL in a Chrome WebDriver, clicks on the
+    'All matches' button, and then selects a country from a dropdown menu based
+    on the provided index. It extracts match data including dates, home teams,
+    scores, and away teams for the selected country, and compiles this information
+    into a pandas DataFrame.
+
+    Args:
+        url (str): The URL of the website to scrape for match data.
+        index (int): The index of the country in the dropdown menu to select.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the scraped data with columns for
+                      date, home team, score, and away team.
+
+    Note:
+        This function includes explicit time delays (using time.sleep()) to allow
+        for dynamic content loading on the page. The webpage structure should contain
+        specific elements identifiable by given XPATHs, IDs, and tags. Changes in the
+        web page's structure may require updates to the locators used in this function.
+    """
+
     print(f'Getting data for country {index}')
 
+    # Initialize the Chrome WebDriver
     driver = webdriver.Chrome()
     driver.get(url)
 
+    # Find and click the 'All matches' button
     all_matches_btn = (driver.find_element(By.XPATH,
                                            '//label[@analytics-event="All matches"]')
                        )
     all_matches_btn.click()
 
+    # Wait for dynamic content to load
     time.sleep(5)
 
+    # Select a country from the dropdown menu
     dropdown = Select(driver.find_element(By.ID,'country'))
     dropdown.select_by_index(index)
 
+    # Wait again for dynamic content to load after selection
     time.sleep(5)
 
+    # Find all match elements
     matches = driver.find_elements(By.TAG_NAME,'tr')
 
+    # Initialize lists to store match data
     dates = []
     home_teams = []
     scores = []
     away_teams = []
 
+    # Iterate through each match and extract data
     for match in matches:
         date = match.find_element(By.XPATH,'./td[1]').text
         home_team = match.find_element(By.XPATH,'./td[2]').text
@@ -107,7 +140,11 @@ def get_data_dropdown(url,index) -> pd:
         home_teams.append(home_team)
         scores.append(score)
         away_teams.append(away_team)
+
+    # Quit the WebDriver session
     driver.quit()
+
+    # Create a DataFrame from the extracted data
     df = pd.DataFrame({
         'dates': dates,
         'home_teams': home_teams,
