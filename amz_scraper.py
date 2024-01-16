208f3345355ccbc83d9ecb6a62349fd0423ae7a2
Mldng <melody.duong.nguyen@gmail.com>
2024-01-16 11:58:57 +0900

    add user-agents list

--- amz_scraper.py
+++ amz_scraper.py
@@ -2,6 +2,7 @@ import csv
 import time
 from selenium.webdriver import Chrome, ChromeOptions
 from selenium.webdriver.common.by import By
+import random
 
 class crawledArticle():
     def __init__(self, title, price):
@@ -9,6 +10,30 @@ class crawledArticle():
         self.price = price
 
 class Bot:
+    def get_browser(self):
+        user_agents = [
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko",
+            "Mozilla/5.0 (iPad; CPU OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1",
+            "Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1",
+            "Mozilla/5.0 (Linux; Android 10; SM-G981B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.91 Mobile Safari/537.36",
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36",
+            "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0"
+        ]
+
+        options = ChromeOptions()
+        # Set a random user-agent from the list
+        options.add_argument(f'user-agent={random.choice(user_agents)}')
+        options.headless = False
+        options.add_experimental_option("detach", True)
+
+        browser = Chrome(options=options)
+        browser.maximize_window()
+        return browser
+
     def article(self, name):
         count = 1 #number of article
         page = 1 #number of pages to be crawled
